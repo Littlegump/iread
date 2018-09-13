ab649e57c90ccf3a0d13e6bf9bfbeb378ef1d296
Littlegump <KaiXuanyin@live.com>
2018-09-13 14:44:17 +0800

    添加src/components

---
+++ src/components/DangerButton.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import MyButton from './MyButton';
+
+class MyDangerButton extends Component {
+  render() {
+    return <Button color='red' />;
+  }
+}
+
+export default MyDangerButton;
---
+++ src/components/MyButton.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class MyButton extends Component {
+  render() {
+    <p> haha</p>
+  }
+}
+
+export default MyButton;
